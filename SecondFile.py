ee16e35bb39a94cf8efc5e905cc0ae4a7e29773e
marcelatessio <marcelatessio4@gmail.com>
2024-12-08 18:30:35 -0300

    Readt to continue

--- SecondFile.py
+++ SecondFile.py
@@ -1,6 +1,6 @@
-
 #Segunda rama creada
 # Para crear una nueva rama: git branch nueva-rama
 # Para moverme a dicha rama: git checkout nueva-rama
 
 # This activities is difficult because not sincronize
+# ready to continue
